dc7dc086c15b8b69dc8177f97eea69b7d14f7206
youssef-ait-elmahi <youssefaitelmahi3@gmail.com>
2023-06-08 20:34:31 +0100

    3. Infinite addition

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+    arguments = len(argv) - 1
+    sum = 0
+    for i in range(arguments):
+        sum += int(argv[i + 1])
+print("{}".format(sum))
