cdf9f344e12f7d056696e91e2cb111ccae9b9814
youssef-ait-elmahi <youssefaitelmahi3@gmail.com>
2023-06-11 21:06:57 +0100

    6. Lists of lists = Matrix

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,6 @@
+#!/user/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for colm in (matrix):
+        for row in (colm):
+            print("{:d}".format(row), end=" " if row != colm[-1] else "")
+        print()
