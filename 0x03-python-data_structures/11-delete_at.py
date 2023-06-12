d765aac9891fb233633be9460a50d79d4e67125c
youssef-ait-elmahi <youssefaitelmahi3@gmail.com>
2023-06-12 20:40:48 +0100

    11. Delete at

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
     helper = my_list[:]
-    if 0 > idx >= len(my_list):
-        return
+    if len(my_list) < idx or idx < 0:
+        return my_list
     else:
         del helper[idx]
         return helper
