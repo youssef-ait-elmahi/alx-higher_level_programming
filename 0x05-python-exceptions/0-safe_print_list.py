c964f60dcf356d1d7f0e4cb95f2c695aff3b36a8
youssef-ait-elmahi <youssefaitelmahi3@gmail.com>
2023-06-26 19:04:02 +0100

    0. Safe list printing

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -9,15 +9,3 @@ def safe_print_list(my_list=[], x=0):
             break
     print(end='\n')
     return(helper)
-
-
-safe_print_list = __import__('0-safe_print_list').safe_print_list
-
-my_list = [1, 2, 3, 4, 5]
-
-nb_print = safe_print_list(my_list, 2)
-print("nb_print: {:d}".format(nb_print))
-nb_print = safe_print_list(my_list, len(my_list))
-print("nb_print: {:d}".format(nb_print))
-nb_print = safe_print_list(my_list, len(my_list) + 2)
-print("nb_print: {:d}".format(nb_print))
