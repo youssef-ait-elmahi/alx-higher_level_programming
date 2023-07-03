173ed67f4b0a0110926dc6bcfd94760f4daccf89
youssef-ait-elmahi <youssefaitelmahi3@gmail.com>
2023-07-03 22:55:23 +0100

    7. Change representation

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -13,7 +13,7 @@ class Rectangle:
     """
     number_of_instances = 0
     print_symbol = "#"
-    
+
     def __init__(self, width=0, height=0):
         """
         Initializes a new instance of the Rectangle class.
