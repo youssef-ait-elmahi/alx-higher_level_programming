1111a8fcc2528493fcc24e73999f06dde84f5d58
youssef-ait-elmahi <youssefaitelmahi3@gmail.com>
2023-07-11 15:52:27 +0100

    task 1

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+""" Module writes to a text file"""
+
+
+def write_file(filename="", text=""):
+    """ Function writes to file
+    Args:
+        filename: filename
+        text: text to write
+    Raises
+        Exception: file can be opened
+    """
+    with open(filename, 'w', encoding="utf-8") as f:
+        return f.write(text)
