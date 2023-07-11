2d147db06d773f797da818faf4521eed30b7e2c5
youssef-ait-elmahi <youssefaitelmahi3@gmail.com>
2023-07-11 16:12:55 +0100

    task 3

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+""" Module returns the JSON of an object"""
+import json
+
+
+def to_json_string(my_obj):
+    """ Function returns JSON of an object
+    Args:
+        my_obj: object
+    Raises:
+        Exception: when the object can't be encoded
+    """
+    return json.dumps(my_obj)
