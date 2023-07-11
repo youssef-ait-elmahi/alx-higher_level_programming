1ffcd79b21ecad294d134f0045f0171fa780b23c
youssef-ait-elmahi <youssefaitelmahi3@gmail.com>
2023-07-11 16:17:03 +0100

    task 5

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+""" Module Object to a text file using JSON"""
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """ Function object to a text file with JSON
+    Args:
+        my_obj: object
+        filename: textfile name
+    Raises:
+        Exception: when the object can't be encoded
+    """
+    with open(filename, 'w', encoding="utf-8") as f:
+        json.dump(my_obj, f)
