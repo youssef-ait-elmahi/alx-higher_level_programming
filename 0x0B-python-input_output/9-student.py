46d40c387c9b24305a5b06aa51329b9d305199d7
youssef-ait-elmahi <youssefaitelmahi3@gmail.com>
2023-07-11 16:36:39 +0100

    task 9

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+""" Module defines the class Student"""
+
+
+class Student:
+    """ Class to create student instances """
+    def __init__(self, first_name, last_name, age):
+        """ Special method to initialize """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """ Method that returns directory description """
+        return self.__dict__.copy()
