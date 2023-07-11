e60a7643424f4cd0475ceda041662be76647759f
youssef-ait-elmahi <youssefaitelmahi3@gmail.com>
2023-07-11 15:50:19 +0100

    task 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+""" Module reads from a file """
+
+
+def read_file(filename=""):
+    """ Function reads file
+    Args:
+        filename: filename
+    Raises
+        Exception: can file be opened
+    """
+    with open(filename, 'r', encoding="utf-8") as f:
+        r = f.read()
+        print(r, end='')
