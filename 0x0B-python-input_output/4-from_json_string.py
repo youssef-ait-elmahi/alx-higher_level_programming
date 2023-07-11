052d2b4d70f07d0597725ff9865b7731132c58f9
youssef-ait-elmahi <youssefaitelmahi3@gmail.com>
2023-07-11 16:14:40 +0100

    task 4

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+""" Module returns an object by as JSON"""
+import json
+
+
+def from_json_string(my_str):
+    """ Function returns an object as JSON
+    Args:
+        my_str: JSON representation
+    Raises:
+        Exception: string can't be decoded
+    """
+    return json.loads(my_str)
