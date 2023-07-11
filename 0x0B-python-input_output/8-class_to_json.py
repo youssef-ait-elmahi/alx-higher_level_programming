3f077d8227f3494aab28f491fe7f4e1b22c789ac
youssef-ait-elmahi <youssefaitelmahi3@gmail.com>
2023-07-11 16:34:25 +0100

    task 8

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+""" Module returns the dictionary"""
+
+
+def class_to_json(obj):
+    """ Function retuns dictionary """
+    dic = {}
+    if hasattr(obj, "__dict__"):
+        dic = obj.__dict__.copy()
+    return dic
