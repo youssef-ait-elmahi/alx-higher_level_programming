21a9e43e4fc7933ea4e42f5c883a9ebc5578c6bb
youssef-ait-elmahi <youssefaitelmahi3@gmail.com>
2023-07-11 15:53:51 +0100

    task 2

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+""" Module appends to a text file"""
+
+
+def append_write(filename="", text=""):
+    """ Function appends to file
+    Args:
+        filename: filename
+        text: text to write
+    Raises
+        Exception: file can be opened
+    """
+    with open(filename, 'a', encoding="utf-8") as f:
+        return f.write(text)
