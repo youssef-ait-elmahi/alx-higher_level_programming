7ac57cfacf6eee563b947693628c6e5bea6e9b18
youssef-ait-elmahi <youssefaitelmahi3@gmail.com>
2023-08-16 16:22:10 +0100

    task 0

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
--- lists all privileges of the MySQL users user_0d_1 and user_0d_2 on your server (in localhost).
+-- Lists all privileges of the MySQL users user_0d_1 and user_0d_2
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
