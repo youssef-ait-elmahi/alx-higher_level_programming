f73f9ecfaf7974d1561952b07e2dd23bac25a99e
youssef-ait-elmahi <youssefaitelmahi3@gmail.com>
2023-09-28 20:59:45 +0100

    task 0 edit

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
-
-# Check if URL has been provided
-if [ "$#" -ne 1 ]; then
-    echo "Usage: $0 <url>"
-    exit 1
-fi
-
-# Send a request to the URL and display the size of the body of the response
+# Takes in a URL, sends a request to that URL, and displays the size of the body of the response
 curl -s "$1" | wc -c
