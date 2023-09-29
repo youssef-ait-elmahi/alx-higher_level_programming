05981e45b1bed22a6eefa76c411c30cbc7fabd76
youssef-ait-elmahi <youssefaitelmahi3@gmail.com>
2023-09-29 18:07:02 +0100

    task 0 edit 2

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,7 +4,7 @@ import urllib.request
 
 url = "https://alx-intranet.hbtn.io/status"
 with urllib.request.urlopen(url) as response:
-    data = response.read()  # this will be the HTML content of the page
+    data = response.read()
     print("- type: {}".format(type(data)))
     print("- content: {}".format(data))
     print("- utf8 content: {}".format(data.decode('utf-8')))
