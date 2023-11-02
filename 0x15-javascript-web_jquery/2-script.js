540d889083a471519a61399986e9e078007a7567
youssef-ait-elmahi <youssefaitelmahi3@gmail.com>
2023-11-02 02:08:08 +0100

    Add click event to change color of header in jQuery.

---
+++ 0x15-javascript-web_jquery/2-script.js
@@ -0,0 +1,6 @@
+// Use jQuery to add a click event to the div with id 'red_header'
+$('#red_header').click(function() {
+    // When the div is clicked, change the color of the header
+    $('header').css('color', '#FF0000');
+  });
+  
